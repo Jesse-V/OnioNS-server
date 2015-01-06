686afbc7e4fa94aded4ebb077bd01bb7925c7081
Jesse Victors <jvictors@jessevictors.com>
2015-01-05 18:21:11 -0700

    scrypt method, base64 encoded output

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 
 #include "main.hpp"
-#include <string>
-#include <iostream>
+#include <cstring>
 #include "libs/libscrypt-1.20/libscrypt.h"
+#include "libs/libscrypt-1.20/b64.h"
+#include "libs/base64.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 
 int main(int argc, char** argv)
 {
-    char* outbuf = new char[512];
-    char* str = const_cast<char*>(std::string("hello world").c_str());
-    libscrypt_hash(outbuf, str, SCRYPT_N, SCRYPT_r, SCRYPT_p);
+    const uint8_t IN_SIZE = 32;
+    uint8_t* input = new uint8_t[IN_SIZE];
+    for (uint8_t j = 0; j < IN_SIZE; j++)
+        input[j] = j;
 
-    std::cout << "hash output: " << outbuf << std::endl;
+    uint8_t* scrypted = new uint8_t[SCRYPT_HASH_LEN];
+    scrypt(input, IN_SIZE, scrypted, SCRYPT_N);
+
+    std::cout << base64_encode(scrypted, SCRYPT_HASH_LEN) << std::endl;
 
     return EXIT_SUCCESS;
+}
+
+
+
+int scrypt(uint8_t* input, uint8_t inputLen, uint8_t* output, uint64_t n)
+{
+    uint8_t* salt = new uint8_t[SCRYPT_SALT_LEN];
+    memset(salt, 0, SCRYPT_SALT_LEN);
+
+    return libscrypt_scrypt(input, inputLen, salt, SCRYPT_SALT_LEN,
+        n, SCRYPT_r, SCRYPT_p, output, SCRYPT_HASH_LEN);
 }
--- main.hpp
+++ main.hpp
@@ -2,6 +2,8 @@
 #ifndef MAIN
 #define MAIN
 
+#include <cstdint>
 
+int scrypt(uint8_t* input, uint8_t inputLen, uint8_t* output, uint64_t n);
 
 #endif
