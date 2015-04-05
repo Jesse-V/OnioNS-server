8f02d35347ccbdaa6cbcb1ecb3939b355cf78eea
Jesse Victors <jvictors@jessevictors.com>
2015-04-04 18:20:34 -0600

    import Tor debian dir

--- pushMainRelease.sh
+++ pushMainRelease.sh
@@ -10,12 +10,12 @@ tar -czf ${name}.orig.tar.gz src/ #http://xkcd.com/1168/
 echo "Tarball creation step complete."
 
 #http://www.cyberciti.biz/faq/linux-unix-creating-a-manpage/
-myManPath="debian/extra_includes"
+myManPath="debian-main/extra_includes"
 gzip --best -c ${myManPath}/manpage > ${myManPath}/OnioNS.1.gz
 echo "Manpage creation step complete."
 
 cd src/
-cp -rl ../debian debian # this cannot be a symlink
+cp -rl ../debian-main debian # this cannot be a symlink
 dpkg-buildpackage -S -sa -kAD97364FC20BEC80
 cd ..
 echo "Debian packaging step complete."
@@ -26,7 +26,7 @@ rm -rf src/debian/
 rm -f ${name}.orig.tar.gz ${name}.debian.tar.gz
 rm -f ${name}.dsc ${name}.dsc ${name}_source.changes
 rm -f ${name}_source.ppa.upload
-rm -f debian/extra_includes/OnioNS.1.gz
+rm -f debian-main/extra_includes/OnioNS.1.gz
 echo "Cleanup complete."
 
 echo "Done pushing packages. Check email for Launchpad build reports."
