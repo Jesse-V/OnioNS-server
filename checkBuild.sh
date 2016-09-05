d8778f7ac116e696698f14513ebfff9ba5527842
Jesse Victors <kernelcorn@torproject.org>
2016-09-05 00:12:36 -0400

    Update build scripts, find required libraries

--- checkBuild.sh
+++ checkBuild.sh
@@ -6,18 +6,26 @@
 # extra analysis, this script can take some time to complete so it's best run
 # infrequently, such as before a release.
 
-# Please install clang-3.6 and cppcheck before running this script.
+# Please install clang-3.8 and cppcheck before running this script.
+# If your distribution has clang-analyzer-3.8, you may need to install that as well.
 
-export CCC_CXX=clang++-3.6
-export CCC_CC=clang-3.6
+# in some distributions the path is /usr/share/clang/scan-build-3.8/c++-analyzer
+export CXX=/usr/share/clang/scan-build-3.8/libexec/c++-analyzer
+export CC=/usr/share/clang/scan-build-3.8/libexec/ccc-analyzer
+
+# delete any previous build as linking fails if there's a mix of compilers
+rm -rf build src/libs/libjson-rpc-cpp/build
 
 mkdir -p build/
 cd build
-scan-build-3.6 cmake ../src -DCMAKE_CXX_COMPILER=/usr/share/clang/scan-build-3.6/c++-analyzer -DCMAKE_C_COMPILER=/usr/share/clang/scan-build-3.6/ccc-analyzer # -DCMAKE_BUILD_TYPE=Debug
+scan-build-3.8 cmake ../src -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC
+# -DCMAKE_BUILD_TYPE=Debug
+
+# 2>&1 | grep -F -v "src/libs"
 
 echo "Compiling with Clang static analysis...  -------------------------------"
 rm -rf /tmp/scan-build-*
-if (scan-build-3.6 -maxloop 16 -enable-checker core -enable-checker cplusplus -disable-checker deadcode -enable-checker security -enable-checker unix make -j $(grep -c ^processor /proc/cpuinfo)) then
+if (scan-build-3.8 -maxloop 16 -enable-checker core -enable-checker cplusplus -disable-checker deadcode -enable-checker security -enable-checker unix make -j $(grep -c ^processor /proc/cpuinfo)) then
 
   if [ $(ls /tmp/scan-build-* | wc -l 2> /dev/null) -gt 0 ]; then
     echo "Failure: static analysis contains reports."
@@ -27,7 +35,8 @@ if (scan-build-3.6 -maxloop 16 -enable-checker core -enable-checker cplusplus -d
   else
     echo "Additional static analysis...        ----------------------------------------------"
     cd ..
-    cppcheck --enable=all --platform=unix64 --inconclusive src/*
+
+    find src -type f -follow -print | grep -F -v "src/libs" | grep -E "\.h|\.c" | cppcheck -i "src/spec/rpc_spec.json" --enable=all --platform=unix64 --inconclusive --file-list=-
 
     echo "Success: compilation and scan-build check successful!"
   fi
--- devBuild.sh
+++ devBuild.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 
+# This script re-styles the code to a format very similar to Chromium's style
+# and then builds the project using the Clang 3.8 compiler. This is the main
+# script to use if you are compiling frequently.
+
+# Please install clang-3.8 and clang-format-3.8 before running this script.
+
 echo "Re-styling code...        ----------------------------------------------"
 
 # make the code-style consistent
 for f in $(find src/ -type f -name "*.c*" | grep -v "libs"); do
-   clang-format-3.6 -style="{BasedOnStyle: chromium, BreakBeforeBraces: Allman, MaxEmptyLinesToKeep: 3}" -i $f
+   clang-format-3.8 -style="{BasedOnStyle: chromium, BreakBeforeBraces: Allman, MaxEmptyLinesToKeep: 3}" -i $f
 done
 # AlignConsecutiveAssignments: true
 for f in $(find src/ -type f -name "*.h*" | grep -v "libs"); do
-   clang-format-3.6 -style="{BasedOnStyle: chromium, BreakBeforeBraces: Allman, MaxEmptyLinesToKeep: 3}" -i $f
+   clang-format-3.8 -style="{BasedOnStyle: chromium, BreakBeforeBraces: Allman, MaxEmptyLinesToKeep: 3}" -i $f
 done
-
-echo "Creating manpage...       ----------------------------------------------"
-
-#date style to match Tor's manpage
-date=$(date +%m\\/%d\\/%Y)
-gzip --best -c src/assets/manpage | sed "s/<DATE>/$date/g" > src/assets/onions-server.1.gz
 
 echo "Preparing build...        ----------------------------------------------"
 
-export CXX=/usr/bin/clang++-3.6
-export CC=/usr/bin/clang-3.6
+export CXX=clang++-3.8
+export CC=clang-3.8
 
-mkdir -p build/
+# set up build
+mkdir -p build
 cd build
-cmake ../src # -DCMAKE_BUILD_TYPE=Debug
+cmake ../src -DCMAKE_BUILD_TYPE=Debug
 
 echo "Compiling...              ----------------------------------------------"
 if (make -j $(grep -c ^processor /proc/cpuinfo)) then
