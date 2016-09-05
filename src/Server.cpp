917bd2cd9c45d3a8e67abf77d2d1bc9ca479ff20
Jesse Victors <kernelcorn@torproject.org>
2016-09-05 00:16:14 -0400

    Update JSON-RPC server implementation
    Communicate with Tor's control port.
    Set and read Contact field data.

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,135 +1,226 @@
 
 #include "Server.hpp"
-#include <spec/AbstractSpecClient.h>
-#include <jsonrpccpp/client/connectors/socks5client.h>
 #include <onions-common/Log.hpp>
+#include <onions-common/Common.hpp>
 #include <onions-common/Constants.hpp>
-#include <chrono>
-#include <thread>
-#include <iostream>
+#include <onions-common/tor_ipc/SocketException.hpp>
+#include <onions-common/CyoEncode/CyoEncode.hpp>
+#include <onions-common/CyoEncode/CyoDecode.hpp>
+#include <botan/base64.h>
+#include <algorithm>
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <pwd.h>
 
-// ------------------------ spec implementation: ------------------------
-
-std::string Server::getSpecVersion()
+Server::Server(jsonrpc::AbstractServerConnector& connector,
+               short socksPort,
+               short controlPort,
+               bool isQuorumNode)
+    : NetworkServerInterface(connector)
 {
-  return "1.0";
-}
-
-
-
-void Server::putRecord(const Json::Value& record)
-{
-  //todo
-}
-
-
-
-Json::Value Server::getRecord(const std::string& domain)
-{
-  //todo
-  Json::Value temp;
-  return temp;
-}
-
-
-
-std::string Server::getSubtree(const std::string& domain)
-{
-  //todo
-  return "temp";
-}
-
-
-
-Json::Value Server::getRecordsSince(int time)
-{
-  //todo
-  Json::Value temp;
-  return temp;
-}
-
-
-// ------------------------ server meta methods: ------------------------
-//https://github.com/Jesse-V/OnioNS-server/blob/a71420f833458b8848745f8334756a825bc55037/src/Mirror.cpp
-
-
-void Server::start()
-{
-  //loadState();
-
-  if (qNode_)
+  if (isQuorumNode)
     Log::get().notice("Running as a Quorum server.");
   else
     Log::get().notice("Running as normal server.");
-    
-  if (!qNode_)
-    pollQuorumAsync();
-    
-    
-  Log::get().notice("Starting server.");
-  while (StartListening())
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  Log::get().warn("Server aborted!");
-  
-  StopListening(); //close daemon
-}
 
-
-
-void Server::pollQuorumAsync()
-{
-  std::thread t([&]()
+  try
   {
-    jsonrpc::Socks5Client torTunnel("localhost", std::to_string(socksPort_), "http://example.onion:" + Const::SERVER_PORT);
-    AbstractSpecClient client(torTunnel);
-    
-    try
-    {
-      while (true)
-      {
-        Json::Value recordList = client.getRecordsSince(lastPoll_);
-        
-        using namespace std::chrono;
-        auto epochT = system_clock::now().time_since_epoch();
-        lastPoll_ = epochT.count() * system_clock::period::num / system_clock::period::den;
-        
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-      }
-      
-      /*
-      std::cout << client.getData("key", 9) << std::endl;
-      std::cout << client.basicGet() << std::endl;
-      client.noArgNotification();
-      Json::Value array;
-      array.append(7);
-      array.append(8);
-      array.append(9);
-      client.tellServer(array, false);*/
-    }
-    catch (jsonrpc::JsonRpcException e)
-    {
-      Log::get().warn(std::string("Client aborted! ") + e.what());
-    }
-  });
-  t.detach();
+    TorController control("127.0.0.1", controlPort);
+    control.authenticateToTor(Common::getWorkingDirectory() +
+                              "control.authcookie");
+    control.waitForBootstrap();
+
+    std::string hostname = getHostname();
+    Log::get().notice("Server is reachable at " + hostname + ":" + std::to_string(Const::SERVER_PORT));
+
+    SHA256_HASH hash;
+    memset(hash.data(), 0, Const::SHA256_LEN);
+    updateContactData(control, false, hash, hostname);
+    Log::get().notice("Announced server availability via Contact field.");
+
+
+/*
+    SHA256_HASH hash;
+    memset(hash.data(), 0, Const::SHA256_LEN);
+    updateContactData(control, false, hash, "3g2upl4pq6kufc4m.onion");
+    Log::get().notice("contact: \"" + control.getSetting("ContactInfo") + "\"");
+
+    ContactData cd;
+    auto data = getContactData(control.getSetting("ContactInfo"), cd);
+    Log::get().notice(std::string("cd: ") + (cd.onDebugNetwork_ ? "true" : "false") + " - " + cd.addr_);
+*/
+
+    // shutting down: control.reloadSettings(); // reload from file
+
+/*
+    auto socket = control.getSocket();
+    *socket << "GETCONF HiddenServiceOptions\r\n";
+    std::string resp;
+    *socket >> resp;
+Log::get().notice("response: \"" + resp + "\"");
+*/
+    /*
+        auto socket = control.getSocket();
+        *socket << "GETCONF Contact\r\n";
+        // GETINFO network-status
+        // ns/all
+        // status/fresh-relay-descs
+        //
+
+        std::string resp;
+        *socket >> resp;
+    Log::get().notice("contact: \"" + contact + "\"");
+    */
+  }
+  catch (SocketException& e)
+  {
+    Log::get().warn("Communication error with Tor! " + e.description());
+  }
+
+  /*
+  todo:
+    getConsensusDocument stuff
+    read ed25519 key?
+  */
 }
 
 
 
-std::string Server::getWorkingDir()
+void Server::commitTicket(const std::string& commit, int version)
 {
-  std::string workingDir(getpwuid(getuid())->pw_dir);
-  workingDir += "/.OnioNS/";
-
-  if (mkdir(workingDir.c_str(), 0750) == 0)
-    Log::get().notice("Working directory successfully created.");
-
-  return workingDir;
 }
 
+
+
+void Server::revealTicket(const Json::Value& record, int version)
+{
+}
+
+
+
+void Server::putRecord(const Json::Value& record, int version)
+{
+}
+
+
+
+Json::Value Server::getRecordWithSubtree(const std::string& domain, int version)
+{
+  Json::Value v;
+  return v;
+}
+
+
+
+std::string Server::getDomain(const std::string& domain, int version)
+{
+  return "address.onion";
+}
+
+
+
+Json::Value Server::getRecordsSince(int time, int version)
+{
+  Json::Value v;
+  return v;
+}
+
+
+
+// arguments: controller, <true/false>, hash, <address>.onion
+bool Server::updateContactData(TorController& control, bool testNetwork, const SHA256_HASH& rootHash, const std::string& onionAddr)
+{
+  //initialize binary
+  uint8_t bin[BIN_SIZE];
+  memset(bin, 0, BIN_SIZE);
+
+  // add root hash and onion address
+  memcpy(bin, rootHash.data(), Const::SHA256_LEN);
+  CyoDecode::Base32::Decode(&bin[Const::SHA256_LEN], onionAddr.c_str(), 16);
+  return updateContactString(control, (testNetwork ? "1" : "0") + Botan::base64_encode(bin, BIN_SIZE));
+}
+
+
+
+bool Server::updateContactString(TorController& control, const std::string& str)
+{
+  control.reloadSettings(); // reload from file
+  std::string origContact = control.getSetting("ContactInfo");
+
+  auto pos = origContact.find("=");
+  origContact = origContact.substr(pos + 1);
+  std::string newContact = origContact + " |" + str;
+  return control.setSetting("ContactInfo", "\"" + newContact + "\"");
+}
+
+
+
+bool Server::getContactData(const std::string& contactStr, Server::ContactData& cd)
+{
+  // check for OnioNS delimiter and the proper length
+  std::size_t pos = contactStr.find_last_of('|');
+  if (pos == std::string::npos || contactStr.size() - pos != 58)
+    return false;
+
+  // get flag and confirm its validity
+  char debug = contactStr[pos + 1];
+  if (debug != '1' && debug != '0')
+  {
+    Log::get().warn("Unexpected Contact flag! \"" + contactStr + "\"");
+    return false;
+  }
+
+  // decode base64 and confirm validity
+  uint8_t bin[BIN_SIZE]; // safe as I already check the length
+  memset(bin, 0, BIN_SIZE);
+  auto decodedSize = Botan::base64_decode(bin, contactStr.substr(pos + 2));
+  if (decodedSize != BIN_SIZE)
+  {
+    Log::get().warn("Unusual base64 length! \"" + contactStr + "\"");
+    return false;
+  }
+
+  // set debug and root hash variables
+  cd.onDebugNetwork_ = debug == '1';
+  memcpy(cd.rootHash_.data(), bin, Const::SHA256_LEN);
+
+  // set hidden service address variable, convert to lowercase
+  char hs_addr[HS_ADDR_BYTES];
+  CyoEncode::Base32::Encode(hs_addr, &bin[Const::SHA256_LEN], HS_ADDR_BYTES);
+  cd.addr_ = std::string(hs_addr) + ".onion";
+  std::transform(cd.addr_.begin(), cd.addr_.end(), cd.addr_.begin(), ::tolower);
+
+  return true;
+}
+
+
+
+std::string Server::getHostname()
+{
+  std::ifstream file(Common::getWorkingDirectory() + "hostname");
+  if (!file)
+    Log::get().error("Unable to open hostname file!");
+
+  std::string hostname;
+  file >> hostname;
+
+  if (hostname.size() != 22) // 16 chars + .onion = 22 chars
+    Log::get().error("Invalid length of hostname file!");
+  return hostname;
+}
+
+/*
+https://github.com/Jesse-V/OnioNS-server/blob/a71420f833458b8848745f8334756a825bc55037/src/Mirror.cpp
+
+void Server::start()
+{
+  // loadState();
+
+  if (!qNode_)
+  {
+    using namespace std::chrono;
+    auto start = steady_clock::now();
+
+    jsonrpc::Socks5Client torTunnel(
+        "localhost", std::to_string(socksPort_),
+        "http://4fotismd7hyxjvsf.onion:" + std::to_string(Const::SERVER_PORT));
+    SpecClient client(torTunnel);
+*/
--- src/Server.hpp
+++ src/Server.hpp
@@ -2,31 +2,42 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include <onions-common/spec/AbstractSpecServer.h>
+#include <onions-common/api/NetworkServerInterface.h>
+#include <onions-common/tor_ipc/TorController.hpp>
+#include <onions-common/Constants.hpp>
 //#include <onions-common/containers/MerkleTree.hpp>
 //#include <onions-common/containers/records/Record.hpp>
 
-class Server : public AbstractSpecServer
+class Server : public NetworkServerInterface
 {
-  public:
-    Server(jsonrpc::AbstractServerConnector& connector, short socksPort, bool qNode) :
-      AbstractSpecServer(connector), socksPort_(socksPort), qNode_(qNode), lastPoll_(0)
-    {};
+ public:
+   struct ContactData
+   {
+     bool onDebugNetwork_;
+     SHA256_HASH rootHash_;
+     std::string addr_;
+   };
 
-    virtual std::string getSpecVersion();
-    virtual void putRecord(const Json::Value& record);
-    virtual Json::Value getRecord(const std::string& domain);
-    virtual std::string getSubtree(const std::string& domain);
-    virtual Json::Value getRecordsSince(int time);
-    
-    void start();
-    void pollQuorumAsync();
-    std::string getWorkingDir();
-    
-  private:
-    short socksPort_;
-    bool qNode_;
-    long lastPoll_;
+  Server(jsonrpc::AbstractServerConnector&, short, short, bool);
+
+  virtual void commitTicket(const std::string&, int);
+  virtual void revealTicket(const Json::Value&, int);
+  virtual void putRecord(const Json::Value&, int);
+  virtual Json::Value getRecordWithSubtree(const std::string&, int);
+  virtual std::string getDomain(const std::string&, int);
+  virtual Json::Value getRecordsSince(int, int);
+
+  bool updateContactData(TorController&, bool, const SHA256_HASH&, const std::string&);
+  bool updateContactString(TorController&, const std::string&);
+  bool getContactData(const std::string&, ContactData&);
+  std::string getHostname();
+
+ private:
+  bool qNode_;
+
+  const unsigned long HS_ADDR_BYTES = 10;
+  const unsigned long BIN_SIZE = Const::SHA256_LEN + HS_ADDR_BYTES;
+
 };
 
 #endif
